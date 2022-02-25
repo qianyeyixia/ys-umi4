b3b2e10d8cabac0258fb47517fd82b22f5441175
wangyi <2851813754@qq.com>
2022-02-25 18:34:04 +0800

    file📦:  添加todo页面

---
+++ src/pages/todo.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: wangyi
+ * @Description: 
+ * @Date: 2022-02-25 18:16:04
+ * @LastEditTime: 2022-02-25 18:16:07
+ */
+import React from 'react';
+
+function Todo() {
+  return (
+    <div>
+      Todo
+    </div>
+  );
+}
+
+export default Todo;
