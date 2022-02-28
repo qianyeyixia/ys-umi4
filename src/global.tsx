6aa8ab3e9859e2a747964431e626f57e5b4849e9
wangyi <2851813754@qq.com>
2022-02-28 14:12:49 +0800

    style💄:  添加全局的layout组件

--- src/global.tsx
+++ src/global.tsx
@@ -2,6 +2,7 @@
  * @Author: wangyi
  * @Description: 
  * @Date: 2022-02-25 16:40:55
- * @LastEditTime: 2022-02-25 17:19:53
+ * @LastEditTime: 2022-02-28 14:09:21
  */
 
+import "antd/dist/antd.less";
